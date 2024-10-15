1cd73c9305fb4835a3f88a32c7b075760c52df1c
lenamin <lenaminn@gmail.com>
2024-10-15 17:20:31 +0900

    index.js 주석설명 추가 및 post 테이블 정의

--- nodejs/08/ch08_02/models/index.js
+++ nodejs/08/ch08_02/models/index.js
@@ -1,21 +1,24 @@
-'use strict';
+'use strict'; // js 문법 엄격히 검사함 
 
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || 'development'; // 노드에 설정된 환경인 .ENV 파일에 이 값이 없으면 'development'에 있는 파일을 적용 
+const config = require(__dirname + '/../config/config.json')[env]; // config.json의 developement 에 지금 config 정보가 담긴 것 
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  // config에 있는 정보 담김 
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+// Model 디렉토리에 설정된 모든 파일을 불러와서 db 객체에 넣어준다. 
+// 이 파일에 모델 파일을 기술해놓으면, 담아줌 
 fs
   .readdirSync(__dirname)
   .filter(file => {
@@ -31,6 +34,7 @@ fs
     db[model.name] = model;
   });
 
+  // 관계 정의해놓으면 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
@@ -40,4 +44,5 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+// 최종적으로는 db 객체에 해당되는 모델들의 정보가 모두 담김 
 module.exports = db;
---
+++ nodejs/08/ch08_02/models/post.js
@@ -0,0 +1,23 @@
+// module을 내보내기 할 함수 
+module.exports = (sequelize, DataTypes) => {
+  // create table Post()
+  const Post = sequelize.define('Post', {
+    // id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: DataTypes.STRING,
+    author: DataTypes.STRING(50),
+    }, {
+      tableName: "Board" // 테이블 이름 지정도 가능 
+  });
+   return Post // Post 객체 반환 
+};
