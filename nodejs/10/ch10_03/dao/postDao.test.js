5d43748aa6111fcdabbfa2dc3431e28d67a0b474
lenamin <lenaminn@gmail.com>
2024-10-17 15:22:03 +0900

    [단위 테스트] Unit test 코드 작성
    jest 설치 및 테스트코드 작성, 실행

---
+++ nodejs/10/ch10_03/dao/postDao.test.js
@@ -0,0 +1,15 @@
+const postDao = require('./postDao');
+
+//첫번째 인자: 무엇을 테스트할 것인가?
+describe("Test Dao", () => {
+  test("should", async () => {
+    const data = {
+      title: "unit test dao", 
+      content: "unit test dao content", 
+      userId: 1, 
+    };
+    const result = await postDao.createPost(data);
+    // data.title과 동일한 결과를 가질 것을 기대함 
+    expect(result.title).toBe(data.title);
+  });
+});
