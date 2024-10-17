7f9377832299471fb426c54e7b7e48a34a09be22
lenamin <lenaminn@gmail.com>
2024-10-17 16:18:29 +0900

    [MVC 패턴] Router 구현

---
+++ nodejs/10/ch10_03/routes/postRoute.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const postController = require("../controllers/postController");
+
+const router = express.Router();
+
+// posts는 이미 있다고 생각, 이 다음에 posts/ 이후의 URL 설정한다고 생각하면 된다 
+router.post('/', postController.createPost);  // POST /posts
+router.get('/', postController.findAllPost);  // GET /posts
+router.get('/:id', postController.findPostById); // GET /post2/1
+router.put('/:id', postController.updatePost); // PUT /post2/1 
+router.delete('/:id', postController.deletePost); // DELETE /posts/1 
+
+module.exports = router; // app.js에서 그대로 사용할 부분 
