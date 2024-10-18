b915005064929a1d2de1b823875f6beb0c14ac0b
남부04 <nambu04@nambu04ui-MacBookPro.local>
2024-10-18 10:36:55 +0900

    [JWT] DAO 구현

---
+++ nodejs/10/ch10_03/dao/userDao.js
@@ -0,0 +1,29 @@
+const models = require("../models");
+
+const createUser = async (data) => {
+    return await models.User.create(data);
+};
+
+// 전체 유저 반환 
+const findAllUsers = async () => {
+    return await models.User.findAll();
+}
+
+const updateUser = async (id, data) => {
+    return await models.User.update(data, {
+        where: { id },
+    });
+};
+
+const findUserByEmail = async (email) => {
+    return await models.User.findOne({
+        where: { email },
+    });
+};
+
+MediaSourceHandle.exports = {
+    createUser,
+    findAllUsers,
+    updateUser,
+    findUserByEmail,
+};
