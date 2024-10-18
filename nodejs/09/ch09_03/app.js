c9860105763ab3445ab0cb15365a977bb34b5ae1
lenamin <lenaminn@gmail.com>
2024-10-18 16:43:40 +0900

    [MongoDB] CRUD 게시판 구현

--- nodejs/09/ch09_03/app.js
+++ nodejs/09/ch09_03/app.js
@@ -57,5 +57,78 @@ app.get("/posts", async (req, res) => {
     }
 });
 
+app.get("/posts/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const post = await Post.findById(id);
+        res.json({ data: post });
+    } catch(e) {
+        res.status(500).json({ error: e });
+    }
+});
+
+// update 
+app.put("/posts/:id", async (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    try {
+        const post = await Post.findByIdAndUpdate(id, { // id로 post 찾은 다음에 update 해라 
+            title,
+            content,
+        });
+        res.status(200).json({ data: post });
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+});
+
+// delete 
+app.delete("/posts/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const post = await Post.findByIdAndDelete(id);
+        res.status(200).json({ data: post });
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+});
+
+// comment 추가하기 
+app.post("/posts/:id/comments", async (req, res) => {
+    const { id } = req.params;
+    const { comment, author } = req.body;
+
+    try {
+        //해당 게시글을 업데이트 
+        const post = await Post.updateOne({
+            _id: id
+        }, {
+            $push: { comments: { comment: comment, author: author } },
+        }
+    );
+        res.status(200).json({ data: post });
+    } catch (e) {
+        res.status(500).json({error: e});
+    }
+})
+
+app.delete("/posts/:id/comments/:cid", async (req, res) => {
+    const { id, cid } = req.params;
+
+    try {
+        const post = await Post.updateOne({
+            _id: id,
+        }, {
+            $pull: { // 객체에서 데이터 뽑아온다 
+                comments: { _id: cid }, 
+            },
+        });
+        res.status(200).json({ data: post });
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+});
+
 app.listen(3000);
 
